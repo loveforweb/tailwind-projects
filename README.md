1b5fe1006a495a99269c9f2d973cfbc2f6d22298
Jag Chumber <jag@loveforweb.com>
2020-09-14 14:39:32 +0100

    Base tailwind project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Demo Projects demonstrating Tailwind css.
+
+Projects will exist on seperate branches. A list of project will be added below as i complete each one.
+
+- No project yet.
---
+++ base/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container h-screen mx-auto px-3">
+      <div className="w-full max-w-md mx-auto my-auto items-center justify-center h-screen flex">
+        <p className="text-sm text-white bg-gray-800 p-2 ">Base Template</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
